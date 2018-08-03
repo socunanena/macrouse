00f6b7c797bdd7d4fc2409b50dacb539762ff346
Natàlia <natalia.cavi@gmail.com>
2018-08-03 15:20:04 +0200

    Moved constant values to external file

---
+++ config/constants.js
@@ -0,0 +1,28 @@
+export const SUBJECT_FACTORS = {
+  male: {
+    base: 66.473,
+    weight: 13.7516,
+    height: 5.0033,
+    age: 6.755,
+  },
+  female: {
+    base: 655.0955,
+    weight: 9.5634,
+    height: 1.8449,
+    age: 4.6756,
+  },
+};
+
+export const EXERCISE_FACTORS = {
+  none: 1.2,
+  low: 1.375,
+  medium: 1.55,
+  high: 1.725,
+  extreme: 1.9,
+};
+
+export const MACROS_CALORIES = {
+  fat: 9,
+  protein: 4,
+  carbs: 4,
+};
--- src/index.js
+++ src/index.js
@@ -1,33 +1,5 @@
-import { mapValues, pickBy, omitBy, reduce } from 'lodash';
-
-const SUBJECT_FACTORS = {
-  male: {
-    base: 66.473,
-    weight: 13.7516,
-    height: 5.0033,
-    age: 6.755,
-  },
-  female: {
-    base: 655.0955,
-    weight: 9.5634,
-    height: 1.8449,
-    age: 4.6756,
-  },
-};
-
-const EXERCISE_FACTORS = {
-  none: 1.2,
-  low: 1.375,
-  medium: 1.55,
-  high: 1.725,
-  extreme: 1.9,
-};
-
-const MACROS_CALORIES = {
-  fat: 9,
-  protein: 4,
-  carbs: 4,
-};
+import { mapValues, reduce } from 'lodash';
+import { SUBJECT_FACTORS, EXERCISE_FACTORS, MACROS_CALORIES } from '../config/constants';
 
 function validateTypes(macroValues) {
   return reduce(
