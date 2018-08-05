58addaec8200031ef3497d9cef505e74b70a1c4b
Natàlia <natalia.cavi@gmail.com>
2018-08-05 21:21:14 +0200

    Used webpack to bundle the library

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'macrouse',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ['transform-object-rest-spread'],
+          },
+        },
+      },
+    ],
+  },
+};
